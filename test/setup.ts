92dcf96df96841430cf3cfb7b25416967507ee6a
dimav <vezhnovetsdima04@gmail.com>
2025-03-16 12:24:14 +0300

    compare

--- test/setup.ts
+++
@@ -1,6 +0,0 @@
-import { loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
-import { ethers, network } from "hardhat";
-import { expect } from "chai";
-import "@nomicfoundation/hardhat-chai-matchers";
-
-export { loadFixture, ethers, expect, network };
